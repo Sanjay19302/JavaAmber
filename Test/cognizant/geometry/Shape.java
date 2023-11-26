7981aec968a4fcb96bd79cdaa5eb3019b9bf4560
Sanjay19302 <149461178+Sanjay19302@users.noreply.github.com>
2023-11-26 20:23:51 +0530

    Add files via upload

---
+++ Test/cognizant/geometry/Shape.java
@@ -0,0 +1,11 @@
+package com.cognizant.geometry;
+import java.util.Scanner;
+
+import com.cognizant.shapes.*;
+public class Shape{
+
+	public static void main(String[] args) {
+		Circle c1=new Circle();
+	}
+
+}
---
+++ Test/cognizant/shapes/Area.java
@@ -0,0 +1,8 @@
+package com.cognizant.shapes;
+import java .util.*;
+public class Area {
+	public static void main(String[] args) { 
+		Circle c2=new Circle(1.5f);
+	}
+
+}
